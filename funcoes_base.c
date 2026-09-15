75baaca852b2b3aa3c02bd0f55bc61f50b56bde1
Victor Hugo <vichugo487@gmail.com>
2026-09-15 13:10:58 -0300

    hdahda

--- funcoes_base.c
+++ funcoes_base.c
@@ -121,9 +121,17 @@ void ScanQuoteString(char *str)
         return binario;
     }
 }
-Regcabecalho Leitura_Cabecalho(FILE *arquivo_binario)
+Regcabecalho Leitura_Cabecalho(FILE *binario)
 
 {
-    
+    Regcabecalho cabecalho; //define o nome da struct
+    // Le todos os dados do registro de cabecalho
+    fseek(binario,0,SEEK_SET);
+    fread(&cabecalho.status,sizeof(char),1,binario);
+    fread(&cabecalho.topo_pilha,sizeof(int),1,binario);
+    fread(&cabecalho.proxRRN,sizeof(int),1,binario);
+    fread(&cabecalho.nroRegRem,sizeof(int),1,binario);
+    fread(&cabecalho.nroPares,sizeof(int),1,binario);
 
+    return cabecalho;
 }
