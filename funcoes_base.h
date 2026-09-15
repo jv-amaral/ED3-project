14d80ece395c4fe2b087d8581dd65cc8235235c8
João Vitor Amaral <jvamaralsantos123@gmail.com>
2026-09-15 19:16:55 -0300

    arrumei um .h

--- funcoes_base.h
+++ funcoes_base.h
@@ -4,6 +4,7 @@
 void BinarioNaTela(char *arquivo_binario);
 void ScanQuoteString(char *str);
 FILE *verificar_arquivo(char *arquivo_binario,char *modo_de_leitura);
-
+RegCabecalho Leitura_Cabecalho(FILE *binario);
+int Leitura_Registro(FILE *binario, Registro *Reg);
 
 #endif
